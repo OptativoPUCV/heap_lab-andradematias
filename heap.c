adb3d806467c1ef557f051e802fd483ea6ee7290
repl.it user <replituser@example.com>
2020-10-17 21:24:08 +0000

    new try

--- heap.c
+++ heap.c
@@ -33,8 +33,8 @@ void heap_pop(Heap* pq){
 }
 
 Heap* createHeap(){
-  Heap *h = (Heap *) malloc (sizeof(Heap));
-  h->heapArray = (heapElem *) malloc (sizeof(heapElem));
+  Heap *h = (Heap*) malloc (sizeof(Heap));
+  h->heapArray = (heapElem*) calloc (3, sizeof(heapElem));
   h->capac = 3;
   h->size = 0;
   return h;
