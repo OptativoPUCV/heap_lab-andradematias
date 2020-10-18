ce90bbe527509bfff4e5f4c236f42ac141101536
repl.it user <replituser@example.com>
2020-10-18 10:38:10 +0000

    new try

--- heap.c
+++ heap.c
@@ -33,11 +33,11 @@ void heap_push(Heap* pq, void* data, int priority){
   pq->heapArray[i].data = data;
   pq->heapArray[i].priority = priority;
   
-  while (pq->heapArray[i].priority < priority && i > 0) {    
-      void *aux_data = pq->heapArray[i].data;
-      int aux_priority = pq->heapArray[i].priority;
-      pq->heapArray[i].data = data;
-      pq->heapArray[i].priority = priority;
+  while (pq->heapArray[(i-1)/2].priority < priority && i > 0) {    
+      void *aux_data = pq->heapArray[(i-1)/2].data;
+      int aux_priority = pq->heapArray[(i-1)/2].priority;
+      pq->heapArray[(i-1)/2].data = data;
+      pq->heapArray[(i-1)/2].priority = priority;
       pq->heapArray[2*i+1].data = aux_data;
       pq->heapArray[2*i+1].priority = aux_priority;
       printf("\n%i\n", i);
