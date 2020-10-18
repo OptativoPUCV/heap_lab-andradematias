92d07f5057f0ff1bbddbfa1cb983a7dcccd1fa88
repl.it user <replituser@example.com>
2020-10-18 08:57:05 +0000

    new try

--- heap.c
+++ heap.c
@@ -42,6 +42,7 @@ void heap_push(Heap* pq, void* data, int priority){
       pq->heapArray[i].priority = priority;
       pq->heapArray[(i-1)/2].data = aux_data;
       pq->heapArray[(i-1)/2].priority = aux_priority;
+      printf("%i", i);
     }
     i = (i-1)/2;
   }
