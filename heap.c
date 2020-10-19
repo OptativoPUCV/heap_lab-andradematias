8954a1747a87dd327126c677c6243d81e5124db1
repl.it user <replituser@example.com>
2020-10-19 21:24:07 +0000

    new try

--- heap.c
+++ heap.c
@@ -57,6 +57,7 @@ void heap_pop(Heap* pq) {
   int i = 0 ;
 
   while (i != pq->size-1) {
+    if (i > pq->size-1) break;
     if (pq->heapArray[i].priority < pq->heapArray[(2*i)+2].priority) {
       void *aux_data = pq->heapArray[i].data;
       int aux_priority = pq->heapArray[i].priority;
