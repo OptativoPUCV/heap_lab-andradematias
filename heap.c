7cdac165fd06438cfa465bb4f023f6839be73133
repl.it user <replituser@example.com>
2020-10-19 03:44:20 +0000

    new try

--- heap.c
+++ heap.c
@@ -60,14 +60,14 @@ void heap_pop(Heap* pq){
   (pq->size)--;
   int i = 0;
 
-  while (i != pq->size) {
-    if (pq->heapArray[i].priority < pq->heapArray[(2*i)+2].priority) {
+  while (i != pq->size-1) {
+    if (pq->heapArray[i].priority < pq->heapArray[(2*i)+1].priority) {
       aux_data = pq->heapArray[i].data;
       aux_priority = pq->heapArray[i].priority;
-      pq->heapArray[i].data = pq->heapArray[(2*i+2)].data;
-      pq->heapArray[i].priority = pq->heapArray[(2*i+2)].priority;
-      pq->heapArray[(2*i+2)].data = aux_data;
-      pq->heapArray[(2*i+2)].priority = aux_priority;   
+      pq->heapArray[i].data = pq->heapArray[(2*i+1)].data;
+      pq->heapArray[i].priority = pq->heapArray[(2*i+1)].priority;
+      pq->heapArray[(2*i+1)].data = aux_data;
+      pq->heapArray[(2*i+1)].priority = aux_priority;   
     }
     i++;
   }
